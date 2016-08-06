efa06d7b02565c7f09cd9f13d58cc2a8ba78f59d
Kendrick Tan <kendricktan0814@gmail.com>
2016-08-06 22:41:51 +1000

    ajax brah

--- disputes/views.py
+++ disputes/views.py
@@ -1,13 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponse
+from django.core.validators import validate_email
 
 def index_view(request):
     return render(request, 'index.html', {})
 
+def landing(request):
+    if request.method == "POST":
+        email = request.POST['email']
+        validate_email(email)
+
+        return HttpResponse('yay')
+
+    return render(request, 'landing.html', {})
 
 def resolve(request):
     return render(request, 'resolve.html', {})
-
 
 def dispute(request):
     return render(request, 'dispute.html', {})
